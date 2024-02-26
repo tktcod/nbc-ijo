549f3cef6b25930af941291f6c1a4ce61ae7e583
jinkshower <askthem@naver.com>
2024-02-26 15:57:26 +0900

    test: 댓글 생성 요청 controllertest
    controllertest를 위한 부모 클래스 설정

---
+++ src/test/java/com/spring/nbcijo/common/ControllerTest.java
@@ -0,0 +1,20 @@
+package com.spring.nbcijo.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.nbcijo.controller.CommentController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(CommentController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+public class ControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+}
