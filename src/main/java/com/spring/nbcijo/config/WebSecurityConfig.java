9c0c76ae85fd1330d097a8265356db703b46c3a5
tktcod <hs060100@gmail.com>
2024-02-28 16:07:51 +0900

    refactor: User관련 api 주소 수정
    Co-authored-by:  jinkshower <askthem@naver.com>

--- src/main/java/com/spring/nbcijo/config/WebSecurityConfig.java
+++ src/main/java/com/spring/nbcijo/config/WebSecurityConfig.java
@@ -79,14 +79,14 @@ public class WebSecurityConfig {
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .permitAll() // resources 접근 허용 설정
                 .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                .requestMatchers("/auth/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                .requestMatchers("/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                 .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         http.formLogin((formLogin) ->
             formLogin
-                .loginPage("/auth/login-page").permitAll()
+                .loginPage("/user/login-page").permitAll()
         );
 
         // 필터 관리
