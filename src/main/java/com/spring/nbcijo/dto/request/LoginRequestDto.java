99303f123ab334ee15df1be35f123ac0151ce7a3
tktcod <hs060100@gmail.com>
2024-02-28 15:57:44 +0900

    refactor: LoginRequestDto annotation 수정
    생성자 annotation 추가

--- src/main/java/com/spring/nbcijo/dto/request/LoginRequestDto.java
+++ src/main/java/com/spring/nbcijo/dto/request/LoginRequestDto.java
@@ -1,9 +1,13 @@
 package com.spring.nbcijo.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
 
     @NotNull
