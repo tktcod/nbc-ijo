57aa45606a4ff486dc92e089f54fce7ce9b86831
tktcod <hs060100@gmail.com>
2024-02-29 10:56:00 +0900

    refactor: login page redirection 수정
    auth-> user 로 api 수정
    AuthController.java 삭제

--- src/main/java/com/spring/nbcijo/controller/UserController.java
+++ src/main/java/com/spring/nbcijo/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "index";
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<ResponseDto<String>> signup(
